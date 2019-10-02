77f6576220ea0c7f321ed8349c7bd3d60ccaa324
Rúben Lício Reis <rubenlr@gmail.com>
2019-10-02 12:28:07 -0300

    Starting parameters tests

--- src/util/axioshelper.js
+++ src/util/axioshelper.js
@@ -17,11 +17,29 @@ const createConfig = (method, endpoint, header, body) => {
   };
 };
 
-export const Fetch = async ({ url, header, body, resp200, resp204 }) => {
+export const Fetch = async ({
+  url,
+  header,
+  body,
+  resp200,
+  resp204,
+  resp20X
+}) => {
   //const config = createConfig("GET", url, header, body);
 
-  const result = await axios.get(url);
+  try {
+    const result = await axios.get(url);
 
-  if (result.status === 200 && resp200) resp200(result.data, result);
-  if (result.status === 204 && resp204) resp204(result.data, result);
+    if (result.status >= 200 && result.status < 300 && resp20X) {
+      resp20X(result.data, result);
+    }
+
+    if (result.status === 200 && resp200) {
+      resp200(result.data, result);
+    } else if (result.status === 204 && resp204) {
+      resp204(result.data, result);
+    }
+  } catch (error) {
+    console.log("Fetch.error", error);
+  }
 };
--- src/util/axioshelper.spec.js
+++ src/util/axioshelper.spec.js
@@ -5,7 +5,7 @@ describe("Axios helper Test", () => {
   describe("FETCH", () => {
     it("Fetch - Some ramdom url", async () => {
       await Fetch({
-        endpoint: "abc123alksdf.com.br"
+        url: "abc123alksdf.com.br"
       });
     });
 
@@ -17,7 +17,7 @@ describe("Axios helper Test", () => {
       mockAxios.get.mockImplementationOnce(() => Promise.resolve(resp));
 
       await Fetch({
-        endpoint: "abc123alksdf.com.br",
+        url: "abc123alksdf.com.br",
         resp200: resp200Mock
       });
 
@@ -34,7 +34,7 @@ describe("Axios helper Test", () => {
       mockAxios.get.mockImplementationOnce(() => Promise.resolve(resp));
 
       await Fetch({
-        endpoint: "abc123alksdf.com.br",
+        url: "abc123alksdf.com.br",
         resp200: resp200Mock,
         resp204: resp204Mock
       });
@@ -42,6 +42,42 @@ describe("Axios helper Test", () => {
       expect(resp200Mock).not.toHaveBeenCalled();
       expect(resp204Mock).toHaveBeenCalled();
       expect(resp204Mock).toBeCalledWith(data, resp);
+    });
+
+    it("Fetch - Should Returns 20X", async () => {
+      const resp200Mock = jest.fn();
+      const resp204Mock = jest.fn();
+      const resp20XMock = jest.fn();
+      const data = { omg: 1 };
+      const resp = { data: data, status: 205 };
+
+      mockAxios.get.mockImplementationOnce(() => Promise.resolve(resp));
+
+      await Fetch({
+        url: "abc123alksdf.com.br",
+        resp200: resp200Mock,
+        resp204: resp204Mock,
+        resp20X: resp20XMock
+      });
+
+      expect(resp200Mock).not.toHaveBeenCalled();
+      expect(resp204Mock).not.toHaveBeenCalled();
+      expect(resp20XMock).toHaveBeenCalled();
+      expect(resp20XMock).toBeCalledWith(data, resp);
+    });
+
+    it("Fetch - Should Pass Parameters Correctly to Axios GET", async () => {
+      const getFn = jest.fn(() => Promise.resolve(resp));
+      const resp = { data: { omg: 1 }, status: 205 };
+
+      mockAxios.get.mockImplementationOnce(getFn);
+
+      await Fetch({
+        url: "abc123alksdf.com.br"
+      });
+
+      expect(getFn).toHaveBeenCalled();
+      expect(getFn).toBeCalledWith("abc123alksdf.com.br");
     });
   });
 });
